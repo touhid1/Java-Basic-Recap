694191321c9d8b234de5e25f89b656fcdf8a0c54
touhid1 <touhidulislam066.com>
2024-01-14 21:21:26 +0600

    Inherit

---
+++ src/test/java/touhidul/InheritExamples.java
@@ -0,0 +1,17 @@
+package touhidul;
+
+public class InheritExamples extends MethodExamples{
+
+	public void printAll() {
+		add(10, 20);
+		multiplication(10, 30, 50);
+		division(10, 20);
+	}
+	
+	public static void main(String[] args) {
+	
+		add(10,20);
+
+	}
+
+}
