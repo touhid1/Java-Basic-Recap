8d0d724d026c7138bfab1fdb7a7876ff27c589ca
touhid1 <touhidulislam066.com>
2024-01-14 21:32:11 +0600

    Variable

---
+++ src/test/java/touhidul/VariableExamples.java
@@ -0,0 +1,22 @@
+package touhidul;
+
+public class VariableExamples {
+	
+	public static void main(String arg[]) {
+		
+		// int, float, double, boolean, String, Array, Object
+		
+		int number1 = 10;
+		int number2 = 20;
+		
+		System.out.println("Number 1: "+number1);
+		System.out.println("Number 2: "+number2);
+		System.out.println("Number 1: "+number1+", Number 2: "+number2);
+		
+		
+		int sum = number1+number2;
+		System.out.println("Summation: "+sum);
+		
+	}
+
+}
