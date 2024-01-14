b9ea5794b3597fec2a2d06b1c88cb0cd4e9ae568
touhid1 <touhidulislam066.com>
2024-01-14 21:29:53 +0600

    Method

---
+++ src/test/java/touhidul/MethodExamples2.java
@@ -0,0 +1,14 @@
+package touhidul;
+
+public class MethodExamples2 {
+	
+	public static void main(String[] args) {
+		
+		MethodExamples obj = new MethodExamples();
+		
+		obj.multiplication(10, 20, 40);
+		
+
+	}
+
+}
